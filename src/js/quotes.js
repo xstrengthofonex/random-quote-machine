c44e2d7e993ce4c06ed278fc82d519f60a4dddb3
Vincent White <xstrengthofonex@gmail.com>
2017-01-15 22:37:57 +0900

    Update quote api

--- src/js/quotes.js
+++ src/js/quotes.js
@@ -18,13 +18,23 @@ function randomColorClass(){
 	return colorClasses[random];
 }
 
+
 function getQuote(){
-	$.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(data){
-		$(".card").addClass(randomColorClass());
-		$(".card").fadeIn();
-		$("#refresh").fadeIn();
-		$("#quote").text(data.quote);
-		$("#author").text(data.author);
+	$.ajax({
+		headers: {
+			'X-Mashape-Key': 'JDiz2qDa3HmshwAtrLdMWP7teES6p17xFsojsnaC17Xq1yOjhG',
+			Accept: 'application/json',
+			'Content-Type': 'application/x-www-form-urlencoded'	
+		},
+		url: "https://andruxnet-random-famous-quotes.p.mashape.com/",
+		dataType: "json",
+		success: function(data){
+			$(".card").addClass(randomColorClass());
+			$(".card").fadeIn();
+			$("#refresh").fadeIn();
+			$("#quote").text(data.quote);
+			$("#author").text(data.author);
+		}
 	});
 }
 
